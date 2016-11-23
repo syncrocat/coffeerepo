0777722bf8ae564429d5cc65293d5c9e7b9a2e68
Grant Zeidler <grantz123@gmail.com>
2016-11-23 16:19:43 -0500

    ...

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+'use strict';
+
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
@@ -5,12 +14,12 @@ const server = express()
 const wss = new SocketServer({ server });
 
 wss.on('connection', (ws) => {
-    console.log('Client connected');
-    ws.on('close', () => console.log('Client disconnected'));
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
 });
 
 setInterval(() => {
-    wss.clients.forEach((client) => {
-        client.send(new Date().toTimeString());
-    });
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
 }, 1000);
