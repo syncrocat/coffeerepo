3b69d6b94d08f732dd2d6c1b03f50fe4a8e0fe58
Grant Zeidler <grantz123@gmail.com>
2016-11-23 16:18:26 -0500

    sigh

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-var HOST = location.origin.replace(/^http/, 'ws');
-var ws = new WebSocket(HOST);
-var el = document.getElementsById('server-time');
-ws.onmessage = function(event) {
-    el.innerHTML = 'Server time: ' + event.data;
-};
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+    ws.on('close', () => console.log('Client disconnected'));
+});
+
+setInterval(() => {
+    wss.clients.forEach((client) => {
+        client.send(new Date().toTimeString());
+    });
+}, 1000);
