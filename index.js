b496c2e5fca594ee6a1bb67c19eb838bfaa42cb9
Grant Zeidler <grantz123@gmail.com>
2016-11-23 16:22:57 -0500

    ???

--- index.js
+++
@@ -1,6 +0,0 @@
-var HOST = location.origin.replace(/^http/, 'ws');
-var ws = new WebSocket(HOST);
-var el = document.getElementsById('server-time');
-ws.onmessage = function(event) {
-    el.innerHTML = 'Server time: ' + event.data;
-};
