ad965bfb0d6738e71fba48e86e251f70443042e8
Grant Zeidler <grantz123@gmail.com>
2016-11-24 11:41:26 -0500

    Testing HTTP GET

--- server.js
+++ server.js
@@ -23,3 +23,29 @@ setInterval(() => {
     client.send(new Date().toTimeString());
   });
 }, 5000);
+
+var http = require('http');
+var options = {
+  host: 'dev.compsawebservices.com',
+  path: '/340/api.php'
+};
+
+var req = http.get(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+  // Buffer the body entirely for processing as a whole.
+  var bodyChunks = [];
+  res.on('data', function(chunk) {
+    // You can process streamed parts here...
+    bodyChunks.push(chunk);
+  }).on('end', function() {
+    var body = Buffer.concat(bodyChunks);
+    console.log('BODY: ' + body);
+    // ...and/or process the entire body here.
+  })
+});
+
+req.on('error', function(e) {
+  console.log('ERROR: ' + e.message);
+});
